c301cf8534b17f7c19f0b8fc5f02a1c4414dbb6b
jhhocs <86144532+jhhocs@users.noreply.github.com>
2021-06-19 00:15:26 -0700

    Add files via upload

---
+++ tempusV1.3.1/Javascript/clock.js
@@ -0,0 +1,30 @@
+setInterval(function (){
+	var currentTime = new Date();
+	var hours = currentTime.getHours();
+	var minutes = currentTime.getMinutes();
+	var seconds = currentTime.getSeconds();
+	var period = "a.m.";
+
+	if (hours >= 12){
+		hours -= 12;
+		period = "p.m.";
+	}
+	
+	if (hours == 0){
+		hours = 12;
+	}
+
+	if (minutes < 10){
+		minutes = "0" + minutes;
+	}
+
+	if (seconds < 10){
+		seconds = "0" + seconds;
+	}
+
+	var clockTime = hours + ":" + minutes + " " + period;
+	var clock = document.getElementById('clock');
+
+	clock.innerText = clockTime;
+
+}, 1000);
